ec8b9a7e77fcdc4c819aafd61b439a3cff77f53d
Morten Jansrud <morten.jansrud@snokam.no>
2023-03-15 15:38:54 +0100

    fix: access

--- Function.cs
+++ Function.cs
@@ -17,7 +17,7 @@ namespace openai
     {
         [FunctionName("conversation")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             OpenAIAPI api = new OpenAIAPI(Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
