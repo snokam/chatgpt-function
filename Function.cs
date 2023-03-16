9643ec59e7dfb7d18d7631de5fdb1f59149f06d0
Morten Jansrud <morten.jansrud@snokam.no>
2023-03-17 00:52:51 +0100

    fix: add async function, total rewrite

--- Function.cs
+++ Function.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using OpenAI_API;
+using OpenAI_API.Chat;
 
 public class DTO{
     public string? answer { get; set; }
@@ -15,20 +14,66 @@ namespace openai
 {
     public static class Function
     {
-        static OpenAIAPI api = new OpenAIAPI(Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
 
-        [FunctionName("conversation")]
-        public static async Task<IActionResult> Run(
+        [FunctionName("syncQuestion")]
+        public static async Task<IActionResult> syncQuestion(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "v1.0/question")] HttpRequest req,
+           ILogger log)
+        {
+            string input = req.Query["input"];
+
+            var result = await ChatGpt.getAnswer(new ChatMessage[] {
+             new ChatMessage(ChatMessageRole.User, input)
+            });
+
+            return new OkObjectResult(new DTO {
+                answer = result.ToString()
+            });
+        }
+
+        [FunctionName("asyncQuestion")]
+        public static async Task asyncQuestion(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "v1.0/question/async")] HttpRequest req,
+           ILogger log)
+        {
+            string input = req.Query["input"];
+            var response = req.HttpContext.Response;
+            await ChatGpt.streamAnswer(response, new ChatMessage[] {
+             new ChatMessage(ChatMessageRole.User, input)
+            });
+        }
+
+        [FunctionName("syncConversation")]
+        public static async Task<IActionResult> syncConversation(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "v1.0/conversation")] HttpRequest req,
             ILogger log)
         {
             string input = req.Query["input"];
-            var chat = api.Chat.CreateConversation();
-            chat.AppendUserInput(input);
-            return new OkObjectResult(new DTO{
-                answer = await chat.GetResponseFromChatbot()
+
+            var result = await ChatGpt.getAnswer(new ChatMessage[] {
+             new ChatMessage(ChatMessageRole.User, input)
+            });
+
+            return new OkObjectResult(new DTO {
+                answer = result.ToString()
             });
         }
+
+        [FunctionName("asyncConversation")]
+        public static async Task asyncConversation(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "v1.0/conversation/async")] HttpRequest req,
+           ILogger log)
+        {
+            string input = req.Query["input"];
+            var response = req.HttpContext.Response;
+            await ChatGpt.streamAnswer(response, new ChatMessage[] {
+             new ChatMessage(ChatMessageRole.User, input)
+            });
+        }
+
+      
     }
+    
+
 }
 
---
+++ Service.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+using System.IO;
+using System.Threading.Tasks;
+using OpenAI_API;
+
+namespace openai
+{
+	public class ChatGpt
+    {
+        static OpenAIAPI api = new OpenAIAPI(Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+
+        public static async Task<ChatResult> getAnswer (ChatMessage[] chatMessages){
+            return await api.Chat.CreateChatCompletionAsync(getChatRequest(chatMessages));
+        }
+
+        public static async Task streamAnswer(HttpResponse response, ChatMessage[] chatMessages)
+        {
+            response.StatusCode = 200;
+            response.ContentType = "text/event-stream";
+            ChatRequest chatRequest = getChatRequest(chatMessages);
+
+            await foreach (ChatResult res in api.Chat.StreamChatEnumerableAsync(chatRequest)) {
+                if (res?.Choices[0]?.Delta?.Content != null) {
+                    await response.WriteAsync(res.Choices[0].Delta.Content);
+                }
+            }
+
+            await response.Body.FlushAsync();
+        }
+
+        public static ChatRequest getChatRequest(ChatMessage[] chatMessages) {
+            return new ChatRequest() {
+                Model = Model.ChatGPTTurbo,
+                Temperature = 0.1,
+                MaxTokens = 200,
+                Messages = chatMessages,
+            };
+        }
+    }
+}
+
